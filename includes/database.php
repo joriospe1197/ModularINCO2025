cb95298bc565ba05619212a03cbd89637ae0e749
Joriospe1197 <gromaheri@gmail.com>
2025-09-30 09:22:42 -0600

    Conexión db railway.

--- includes/database.php
+++ includes/database.php
@@ -1,10 +1,16 @@
 <?php
 
-$db = mysqli_connect('gondola.proxy.rlwy.net', 'root', 'hgVvISLFXzOyIALjHttvbipzQmSCPMAl', 'constructora');
+$db = mysqli_connect(
+    'gondola.proxy.rlwy.net',     // Host
+    'root',                        // Usuario
+    'hgVvISLFXzOyIALjHttvbipzQmSCPMAl', // Contraseña
+    'railway',                     // Nombre de base de datos correcto
+    59369                          // Puerto personalizado
+);
 
 if (!$db) {
-    echo "Error: No se pudo conectar a MySQL.";
-    echo "errno de depuración: " . mysqli_connect_error();
+    echo "Error: No se pudo conectar a MySQL.<br>";
+    echo "errno de depuración: " . mysqli_connect_errno() . "<br>";
     echo "error de depuración: " . mysqli_connect_error();
     exit;
 }
