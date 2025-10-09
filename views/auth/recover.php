297175d0bcb98a49675e5cb5c2e117ae37bdc4c3
Joriospe1197 <gromaheri@gmail.com>
2025-10-08 21:36:40 -0600

    color cyan en recover

--- views/auth/recover.php
+++ views/auth/recover.php
@@ -1,36 +1,35 @@
 <div class="login">
     <div class="contenedor recover">
-    <?php include_once __DIR__ .'/../templates/nombre-sitio.php'; ?>
+        <?php include_once __DIR__ .'/../templates/nombre-sitio.php'; ?>
         
         <div class="contenedor-sm">
             <p class="descripcion-pagina">Coloca tu nuevo password</p>
 
             <?php include_once __DIR__ .'/../templates/alertas.php'; ?>
 
-            <?php if($mostrar) { ?> <!-- En caso de que la variable mostrar este como true, muestra el formulario -->
+            <?php if($mostrar) { ?> <!-- En caso de que la variable mostrar esté en true, muestra el formulario -->
 
-            <form class="formulario" method="POST">
-                <div class="campo">
-                    <!-- <label for="password">Contraseña</label> -->
-                    <input
-                        type="password"
-                        id="contrasena"
-                        placeholder="Ingrese su nueva contraseña."
-                        name="contrasena"
-                    />
-                </div>
+                <form class="formulario" method="POST">
+                    <div class="campo">
+                        <!-- <label for="password">Contraseña</label> -->
+                        <input
+                            type="password"
+                            id="contrasena"
+                            placeholder="Ingrese su nueva contraseña."
+                            name="contrasena"
+                        />
+                    </div>
 
-                <input type="submit" class="boton" value="Guardar Password">
-            </form>
+                    <input type="submit" class="boton" value="Guardar Password">
+                </form>
 
             <?php } ?>
 
-            <div class="recover">
-                <div class="acciones">
-                    <a href="/register">¿No tienes cuenta? obtener una</a>
-                    <a href="/forgot_my_password">¿Olvidaste tu Password?</a>
-                </div>
+            <!-- ✅ Aquí separamos .acciones del .recover anidado -->
+            <div class="acciones">
+                <a href="/register">¿No tienes cuenta? obtener una</a>
+                <a href="/forgot_my_password">¿Olvidaste tu Password?</a>
             </div>
-        </div><!--.contenedor-sm -->
+        </div><!-- .contenedor-sm -->
     </div>
 </div>
