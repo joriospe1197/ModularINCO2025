6b4f518acc8e8d2d4dfe95a7efe2f1c43ff320ec
Joriospe1197 <gromaheri@gmail.com>
2025-10-14 22:35:05 -0600

    Se cambio el dominio.

--- views/dashboard/chat.php
+++ views/dashboard/chat.php
@@ -34,7 +34,7 @@ $idEmpleado = isset($_SESSION['idempleado']) ? $_SESSION['idempleado'] : 0;
 <script src="https://cdn.socket.io/4.8.1/socket.io.min.js"></script>
 
 <script>
-    const socket = io('https://modularinco2025-2.onrender.com/');
+    const socket = io('https://node-app-js-lton.onrender.com');
     const nombreUsuario = <?php echo json_encode($nombreUsuario); ?>;
     const idEmpleado = <?php echo json_encode($idEmpleado); ?>;
 
