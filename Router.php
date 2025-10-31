6782865821dd0fbd01b2a8b2a4773417e2d82052
Joriospe1197 <gromaheri@gmail.com>
2025-10-30 20:08:24 -0600

    Ajuste para que se puede agregar un nuevo producto.

--- Router.php
+++ Router.php
@@ -40,17 +40,20 @@ class Router
 
     public function render($view, $datos = [])
     {
-
-        // Leer lo que le pasamos  a la vista
+        // Extraer las variables del arreglo $datos
         foreach ($datos as $key => $value) {
-            $$key = $value;  // Doble signo de dolar significa: variable variable, básicamente nuestra variable sigue siendo la original, pero al asignarla a otra no la reescribe, mantiene su valor, de esta forma el nombre de la variable se asigna dinamicamente
+            $$key = $value;
         }
 
-        ob_start(); // Almacenamiento en memoria durante un momento...
+        ob_start(); // Inicia el buffer
 
-        // entonces incluimos la vista en el layout
-        include_once __DIR__ . "/app/views/$view.php";
-        $contenido = ob_get_clean(); // Limpia el Buffer
-        include_once __DIR__ . '/app/views/layout.php';
+        // Incluir la vista solicitada
+        include_once __DIR__ . "/../views/$view.php";
+
+        $contenido = ob_get_clean(); // Captura y limpia el buffer
+
+        // Incluir el layout principal
+        include_once __DIR__ . '/../views/layout.php';
     }
+
 }
